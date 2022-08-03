1c022ab1a044a80d278b55800da97060852c60ce
anmeza <anmeza@ucsd.edu>
2022-08-03 13:40:43 -0700

    Update quantize_and_bitflip approach

--- fkeras/utils.py
+++ fkeras/utils.py
@@ -43,71 +43,115 @@ def get_fault_indices(i_tensor, num_faults):
     
     return list(indices)
 
+def lb_index_to_wb_index(i_lbi, i_bit_width, little_endian=True):
+    """
+    Translates a layer-bit index (LBI) to a weight-bit index (WBI)
+    
+    Example:
+    If you have a layer with two 3-bit weights
+    
+    LBI =    0     1     2     3     4     5
+          |  0  |  0  |  0  |  0  |  0  |  0  |
+    WBI =  (0,0) (0,1) (0,2) (1,0) (1,1) (1,2) [   big-endian]
+    WBI =  (0,2) (0,1) (0,0) (1,2) (1,1) (1,0) [little-endian]     
+    """
+    if little_endian:
+        return (int(i_lbi/i_bit_width), (i_bit_width-1) - (i_lbi%i_bit_width))
+    else:
+        return (int(i_lbi/i_bit_width), i_lbi%i_bit_width)
+    
+def wb_index_to_lb_index(i_wbi, i_bit_width, little_endian=True):
+    """
+    Translates a weight-bit index (WBI) to a layer-bit index (LBI)
+    
+    Example:
+    If you have a layer with two 3-bit weights
+    
+    LBI =    0     1     2     3     4     5  
+          |  0  |  0  |  0  |  0  |  0  |  0  |
+    WBI =  (0,0) (0,1) (0,2) (1,0) (1,1) (1,2) [   big-endian]
+    WBI =  (0,2) (0,1) (0,0) (1,2) (1,1) (1,0) [little-endian]    
+    """
+    if little_endian:
+        return (i_wbi[0]*i_bit_width) + (i_bit_width - i_wbi[1]-1)
+    else:
+        return (i_wbi[0]*i_bit_width) + (i_wbi[1])
 
-def quantize_and_bitflip(i_values, i_quantizer, pos=0, ber=0.0):
+def quantize_and_bitflip(i_values, i_quantizer, regions, bers):
     """
     i_values:    a float matrix of non-quantized model parameters
     i_quantizer: qkeras quantizer
-    """ 
+    """
+    # TODO: i_value should be matrix of weights/a kernel. Therefore, we should
+    # 1) quantize everything, 2) select the weights to be fault injected, 3)
+    # inject faults, 4) write fault-injected weights back into quantized kernel
+    
     #S: Get quantization configuration
     quant_config = i_quantizer.get_config()
     scaling_exponent = quant_config["bits"] - quant_config["integer"]
     
-    #S: Determine the number of faults to inject
-    num_faults = int(get_tensor_size(i_values) * quant_config["bits"] * ber)
-    # vvvvvvvvvv - Number of faults determined on weight level
-    # num_faults = int(get_tensor_size(i_values) * ber) 
+    # #S: Determine the number of faults to inject
+    # #S: TODO: Update the way we determine the num_faults
+    # # num_faults = int(get_tensor_size(i_values) * quant_config["bits"] * ber)
+    # num_faults = int(get_tensor_size(i_values) * ber)
+    # print(f"Num_faults: {num_faults}")
     
     #S: Get quantized values (represented as floats)
     result = i_quantizer(i_values)
     
     #S: Flatten i_values
     result = result.numpy().flatten()
+    print(f"Flattened Result = {result}")
     
-    #TODO: Turn for-loop into while-loop so that
-    #####  we iterate through bits instead of weights
-    for i in range(num_faults):
-        curr_val = result[i]
-        # print(f"FL#1: curr_val = {curr_val} | {i}")
+
+    #S:  Iterate through every region
+    for r_i, region in enumerate(regions):
         
-        # TODO: Make this stuff below its own function
-        #S: Turn float to fixed-point representation (an integer)
-        curr_val = float_to_fp(curr_val, scaling_exponent)
-
-        #S: Integer to binary string
-        curr_val = int_to_binstr(curr_val, quant_config["bits"])
-
-        #S: Update position to be little-endian
-        curr_pos = -1 - pos 
-
-        #S: Turn binary string to list of characters
-        curr_val = list(curr_val)
-
-        #S: Flip the indicated bit
-        curr_val[curr_pos] = "0" if "1" == curr_val[curr_pos] else "1"
-
-        #S: Turn list of characters to binary string
-        curr_val = "".join(curr_val)
-
-        #S: Turn binary string to integer
-        curr_val = binstr_to_int(curr_val)
-
-        #S: Turn to integer into float
-        curr_val = fp_to_float(curr_val, scaling_exponent)
+        #S: Determine the number of bits in region
+        ### Assumptions: (1) region bounds are inclusive
+        ### and (2) bounds are in layer-bit index form
+        num_rbits = region[1] - region[0] + 1
         
-        #S: Update i_values/result
-        result[i] = curr_val
+        #S: Determine the number of faults in region
+        num_rfaults = int(num_rbits * bers[r_i])
+        
+        for lbi in range(region[0], region[0]+num_rfaults):
+            #S: Translate layer-bit index to weight-bit index
+            w_i, b_pos = lb_index_to_wb_index(lbi, quant_config["bits"], True)
+            
+            #S: Get the weight containing the bit
+            ### to be flipped/updated
+            curr_val = result[w_i]
+
+            #S: Turn float to fixed-point representation (an integer)
+            curr_val = float_to_fp(curr_val, scaling_exponent)
+
+            #S: Integer to binary string
+            curr_val = int_to_binstr(curr_val, quant_config["bits"])
+
+            #S: Update position to be little-endian
+            curr_pos = -1 - b_pos 
+
+            #S: Turn binary string to list of characters
+            curr_val = list(curr_val)
+
+            #S: Flip the indicated bit
+            curr_val[curr_pos] = "0" if "1" == curr_val[curr_pos] else "1"
+
+            #S: Turn list of characters to binary string
+            curr_val = "".join(curr_val)
+
+            #S: Turn binary string to integer
+            curr_val = binstr_to_int(curr_val)
+
+            #S: Turn to integer into float
+            curr_val = fp_to_float(curr_val, scaling_exponent)
+
+            #S: Update i_values/result
+            result[w_i] = curr_val
         
         
     #S: Reshape i_values
     result = tf.reshape(result, i_values.shape)
     
     return result
-
-# For later, do things at weight-level?
-# If you want to specify bit position(s), then ber = 1 and you must provide a Weight error
-# rate. 
-
-# If you want to specify bit region + ber, then provide (list of ) bit regions
-# and a (list of) bit error rates, where bit region at index i has bit error
-# rate at index i. 
--- test/test_quantize_and_bitflip.py
+++ test/test_quantize_and_bitflip.py
@@ -14,13 +14,26 @@ def create_test_tensors():
 def test_bitflip_zero_tensor():
     golden_tensor = tf.fill((2, 2, 4), 0.5)
     test_quantizer = quantized_bits(bits=8, integer=7, keep_negative=1, alpha=1) 
-    test_tensor = tf.zeros((2,2,4)) 
+    test_tensor = tf.zeros((2,2,4))
+    
+    #TODO: Update the following code block with function call that
+    ###### returns the same lbi region
+    my_bit_pos = a_test_position
+    my_test_regions = list()
+    my_test_bers = list()
+    for i in range(0,16):
+        inclusive_reg_start = wb_index_to_lb_index((i,my_bit_pos), 8)
+        inclusive_reg_end   = wb_index_to_lb_index((i,my_bit_pos), 8)
+        my_test_regions.append( (inclusive_reg_start, inclusive_reg_end) )
+        my_test_bers.append(1.0)
+
     qb_tensor = fk.utils.quantize_and_bitflip(
         test_tensor, 
         test_quantizer, 
-        pos=0, 
-        ber=1
-    ) 
+        my_test_regions, 
+        my_test_bers
+    )
+    
     assert equal_tensors(qb_tensor, golden_tensor)
 
 def test_bitflip_zero_tensor_pos_2():
@@ -29,12 +42,24 @@ def test_bitflip_zero_tensor_pos_2():
 def test_zero_ber():
     golden_tensor = tf.fill((2, 2, 4), 0.0)
     test_tensor = tf.zeros((2,2,4)) 
-    test_quantizer = quantized_bits(bits=8, integer=7, keep_negative=1, alpha=1) 
+    test_quantizer = quantized_bits(bits=8, integer=7, keep_negative=1, alpha=1)
+    
+    #TODO: Update the following code block with function call that
+    ###### returns the same lbi region
+    my_bit_pos = a_test_position
+    my_test_regions = list()
+    my_test_bers = list()
+    for i in range(0,16):
+        inclusive_reg_start = wb_index_to_lb_index((i,my_bit_pos), 8)
+        inclusive_reg_end   = wb_index_to_lb_index((i,my_bit_pos), 8)
+        my_test_regions.append( (inclusive_reg_start, inclusive_reg_end) )
+        my_test_bers.append(0.0)
+
     qb_tensor = fk.utils.quantize_and_bitflip(
         test_tensor, 
         test_quantizer, 
-        pos=0, 
-        ber=0
+        my_test_regions, 
+        my_test_bers
     )
     assert equal_tensors(qb_tensor, golden_tensor)
 
