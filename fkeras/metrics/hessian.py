WIP hessian fix

fkeras/metrics/hessian.py
@@ -600,7 +600,7 @@ class HessianMetrics:
         flattened_grads = list()
         for g in grads:
             print(f"grad shape: {g.shape}")
-            flattened_grads += g.flatten()
+            flattened_grads.extend(g.flatten())
         grads = np.array(flattened_grads)
 
         print(f"flat grads shape: {grads.shape}")